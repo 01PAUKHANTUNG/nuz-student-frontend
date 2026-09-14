23a69064f11560538b785f52a77e91f7e53d3af0
Peter Tung <pktung470@gmail.com>
2026-09-14 19:47:00 +0530

    bakendUrlaive

--- src/context/NUZContext.tsx
+++ src/context/NUZContext.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useState } from "react";
 import axios from 'axios'
 
 
-const backendUrl = "http://localhost:5000"
+const backendUrl = process.env.EXPO_PUBLIC_BACKEND_URL;
 
 type NUZContextType = {
   token: string;
